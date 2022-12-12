000822c25fdfbfd4b960fc994861e52576f85dd8
Jalil <seth.abdeljalil@gmail.com>
2022-12-12 17:07:40 +0100

    Marrakech

--- resources/views/admin/regions/create.blade.php
+++ resources/views/admin/regions/create.blade.php
@@ -11,21 +11,14 @@
                 <div class="panel-body">
                     <form method="POST" action="{{ route("admin.regions.store") }}" enctype="multipart/form-data">
                         @csrf
-                        <div class="form-group {{ $errors->has('ville') ? 'has-error' : '' }}">
-                            <label for="ville">{{ trans('cruds.region.fields.ville') }}</label>
-                            <input class="form-control" type="text" name="ville" id="ville" value="{{ old('ville', '') }}">
-                            @if($errors->has('ville'))
-                                <span class="help-block" role="alert">{{ $errors->first('ville') }}</span>
-                            @endif
-                            <span class="help-block">{{ trans('cruds.region.fields.ville_helper') }}</span>
-                        </div>
+                        <input type="hidden" value="Marrakech" name="ville">
                         <div class="form-group {{ $errors->has('regions') ? 'has-error' : '' }}">
-                            <label for="regions">{{ trans('cruds.region.fields.regions') }}</label>
+                            <label for="regions">Region</label>
                             <input class="form-control" type="text" name="regions" id="regions" value="{{ old('regions', '') }}">
                             @if($errors->has('regions'))
                                 <span class="help-block" role="alert">{{ $errors->first('regions') }}</span>
                             @endif
-                            <span class="help-block">{{ trans('cruds.region.fields.regions_helper') }}</span>
+                            <span class="help-block"></span>
                         </div>
                         <div class="form-group">
                             <button class="btn btn-danger" type="submit">
