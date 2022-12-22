e00ef5b09442aeadf6d91b140916ed5a1733c646
Jalil <seth.abdeljalil@gmail.com>
2022-12-22 05:08:30 +0100

    new

--- resources/views/admin/orders/show.blade.php
+++ resources/views/admin/orders/show.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.admin')
 @section('content')
-<style>
+    <style>
         .slide-whatsapp {
             color: #025f06 !important;
         }
@@ -8,10 +8,11 @@
         .slide-phone {
             color: #008ff8 !important;
         }
+
         #icon {
             margin-right: 6px;
         }
-</style>
+    </style>
     <div class="content">
 
         <div class="row">
@@ -31,48 +32,51 @@
                                 <div class="col-md-6">
                                     <table class="table table-bordered table-striped">
                                         <tbody>
-                                            <form action="/admin/orders/changeOrder" method="post" >
+                                            <form action="/admin/orders/changeOrder" method="post">
                                                 @csrf
 
-                                            <tr id="{{$order->status}}">
-                                                <th >
-                                                    Status
-                                                </th>
-                                                <td>
-                                                    <select name="status" id="status" class="custom-select rounded-0">
-                                                        @can('Livreur') @php $status_orders = $status_orders2 @endphp @endcan
-                                                        @foreach ($status_orders as $s)
-                                                            <option value="{{ $s }}"
-                                                                @if ($order->status == $s) selected @endif>
-                                                                {{ ucfirst($s) ?? '' }}</option>
-                                                        @endforeach
-                                                    </select>
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <th>
-                                                    Reference
-                                                </th>
-                                                <td>
-                                                    {{ $order->ref ?? '' }}
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <th>
-                                                    Temps de commande
-                                                </th>
-                                                <td>
-                                                    {{ $order->created_at }}
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <th>
-                                                    Region
-                                                </th>
-                                                <td>
-                                                    {{ \App\Models\Region::where('id',\App\Models\Client::where('id', $order->client_id)->pluck('region_id')->first())->pluck('regions')->first() ?? '' }}
-                                                </td>
-                                            </tr>
+                                                <tr id="{{ $order->status }}">
+                                                    <th>
+                                                        Status
+                                                    </th>
+                                                    <td>
+                                                        <select name="status" id="status"
+                                                            class="custom-select rounded-0">
+                                                            @can('Livreur')
+                                                                @php $status_orders = $status_orders2 @endphp
+                                                            @endcan
+                                                            @foreach ($status_orders as $s)
+                                                                <option value="{{ $s }}"
+                                                                    @if ($order->status == $s) selected @endif>
+                                                                    {{ ucfirst($s) ?? '' }}</option>
+                                                            @endforeach
+                                                        </select>
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <th>
+                                                        Reference
+                                                    </th>
+                                                    <td>
+                                                        {{ $order->ref ?? '' }}
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <th>
+                                                        Temps de commande
+                                                    </th>
+                                                    <td>
+                                                        {{ $order->created_at }}
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <th>
+                                                        Region
+                                                    </th>
+                                                    <td>
+                                                        {{ \App\Models\Region::where('id',\App\Models\Client::where('id', $order->client_id)->pluck('region_id')->first())->pluck('regions')->first() ?? '' }}
+                                                    </td>
+                                                </tr>
                                         </tbody>
                                     </table>
 
@@ -85,11 +89,14 @@
                                                 </th>
                                                 <td>
                                                     @if ($order->livreur_id != '' and isset(\App\Models\Livreur::find($order->livreur_id)->photo))
-                                                        <img src="{{\App\Models\Livreur::find($order->livreur_id)->photo->getUrl('thumb')}}" width="40px" class="img-circle elevation-2" srcset="">
+                                                        <img src="{{ \App\Models\Livreur::find($order->livreur_id)->photo->getUrl('thumb') }}"
+                                                            width="40px" class="img-circle elevation-2" srcset="">
                                                     @else
-                                                        <img src="{{asset('noimg.jpeg')}}" width="40px" class="img-circle elevation-2" srcset="">
+                                                        <img src="{{ asset('noimg.jpeg') }}" width="40px"
+                                                            class="img-circle elevation-2" srcset="">
                                                     @endif
-                                                    <select name="livreur" id="livreur" class="custom-select rounded-0" @can('Livreur') disabled @endcan>
+                                                    <select name="livreur" id="livreur" class="custom-select rounded-0"
+                                                        @can('Livreur') disabled @endcan>
                                                         <option value="">Selectionner un livreur</option>
                                                         @foreach ($livreurs as $liv)
                                                             <option value="{{ $liv->id }}"
@@ -101,11 +108,12 @@
                                             </tr>
                                             <tr>
                                                 <th>
-                                                    
+
                                                 </th>
                                                 <td>
-                                                    <input type="hidden" name="order_id" value="{{$order->id}}">
-                                                    <button type="submit" class="btn btn-success" style="float:right">Enregistrer</button>
+                                                    <input type="hidden" name="order_id" value="{{ $order->id }}">
+                                                    <button type="submit" class="btn btn-success"
+                                                        style="float:right">Enregistrer</button>
                                                 </td>
                                             </tr>
                                         </tbody>
@@ -129,16 +137,24 @@
                                                     Telephone
                                                 </th>
                                                 <td>
-                                                    <a href="tel:{{ str_replace(' ', '', str_replace(' ', '', $order->client->phone)) }}"><i
-                                                        class="fa-solid fa-2x fa-phone-volume slide-phone"></i> Appeler</a>
+                                                    <a
+                                                        href="tel:{{ str_replace(' ', '', str_replace(' ', '', $order->client->phone)) }}"><i
+                                                            class="fa-solid fa-2x fa-phone-volume slide-phone"></i>
+                                                        Appeler</a>
                                                     : {{ $order->client->phone ?? '' }}
+
+                                                    <hr>
+                                                    <a href="https://api.whatsapp.com/send?phone=212{{ ltrim($order->client->phone, 'g') }}"
+                                                        target="_blank"><i
+                                                            class="fa-brands fa-2x  fa-whatsapp  slide-whatsapp"></i>
+                                                        Whatsapp ( sans contenu )</a>
                                                     <hr>
                                                     <a href="https://api.whatsapp.com/send?phone=212{{ ltrim($order->client->phone, 'g') }}&text=Bonjour, vous avez ef
-                                                        fectuez une commande chez TopTopDonuts.ma de {{ $order->total + floatval($order->livraison) ?? '' }} DH, référence de la commande : ( {{$order->ref}} ), Merci de consulter le lien https://{{$host}}/track/{{$order->ref}} pour voir l'état de votre commande."
-                                                            target="_blank"><i class="fa-brands fa-2x  fa-whatsapp  slide-whatsapp"></i> Whatsapp ( avec contenu ) <i class="fa-sharp fa-2x fa-solid fa-envelope-open-text"></i></a>
-                                                     <hr>
-                                                    <a href="https://api.whatsapp.com/send?phone=212{{ ltrim($order->client->phone, 'g') }}"
-                                                            target="_blank"><i class="fa-brands fa-2x  fa-whatsapp  slide-whatsapp"></i> Whatsapp ( sans contenu )</a>
+                                                        fectuez une commande chez TopTopDonuts.ma de {{ $order->total + floatval($order->livraison) ?? '' }} DH, référence de la commande : ( {{ $order->ref }} ), Merci de consulter le lien https://{{ $host }}/track/{{ $order->ref }} pour voir l'état de votre commande."
+                                                        target="_blank"><i
+                                                            class="fa-brands fa-2x  fa-whatsapp  slide-whatsapp"></i>
+                                                        Whatsapp ( avec contenu ) <i
+                                                            class="fa-sharp fa-2x fa-solid fa-envelope-open-text"></i></a>
                                                 </td>
                                             </tr>
                                             <tr>
@@ -163,14 +179,14 @@
                                     <table class="table table-bordered table-striped">
                                         <tbody>
                                             @if ($order->notes)
-                                            <tr   style="background-color: rgb(248, 255, 215)">
-                                                <th >
-                                                    Notes
-                                                </th>
-                                                <td>
-                                                    {{ $order->notes ?? '' }}
-                                                </td>
-                                            </tr>
+                                                <tr style="background-color: rgb(248, 255, 215)">
+                                                    <th>
+                                                        Notes
+                                                    </th>
+                                                    <td>
+                                                        {{ $order->notes ?? '' }}
+                                                    </td>
+                                                </tr>
                                             @endif
 
                                             <tr>
@@ -195,7 +211,7 @@
                             </div>
                             <div class="row">
                                 <div class="col-md-12">
-                                    <div >
+                                    <div>
                                         <div class="table-responsive">
                                             <table
                                                 class=" table table-bordered table-striped table-hover datatable datatable-Order">
@@ -234,23 +250,23 @@
                                                         <td>
                                                             <b>{{ $order->total }} DH</b>
                                                         </td>
-                                                     </tr>
-                                                     <tr style="background-color: #fff">
+                                                    </tr>
+                                                    <tr style="background-color: #fff">
                                                         <td colspan="2">
                                                             <b>Livraison</b>
                                                         </td>
                                                         <td>
-                                                            <b>{{ $order->livraison }} DH</h3
-                                                        </td>
-                                                     </tr>
-                                                     <tr style="background-color: #fff">
+                                                            <b>{{ $order->livraison }} DH</h3 </td>
+                                                    </tr>
+                                                    <tr style="background-color: #fff">
                                                         <td colspan="2">
                                                             <b>Total</b>
                                                         </td>
                                                         <td>
-                                                            <b>{{ $order->total + floatval($order->livraison) ?? '' }} DH</b>
+                                                            <b>{{ $order->total + floatval($order->livraison) ?? '' }}
+                                                                DH</b>
                                                         </td>
-                                                     </tr>
+                                                    </tr>
                                                 </tbody>
                                             </table>
                                         </div>
