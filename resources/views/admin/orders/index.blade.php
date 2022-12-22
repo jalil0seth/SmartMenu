890ba03bf919774574a2d972e515c96de5b7872f
Jalil <seth.abdeljalil@gmail.com>
2022-12-22 03:55:12 +0100

    new

--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -35,7 +35,7 @@
     <div class="content">
         <div class="row">
             <div class="col-lg-12">
-                <button class="btn btn-lg btn-success" style="margin:4px" onclick="notif(this)"><i class="fa-solid fa-bell" style="animation: skew-y-shakeing 1.5s infinite;""></i> Réactiver le son ( Notifications ) </button>
+                <button class="btn btn-lg btn-success" style="margin:5px" onclick="notif(this)"><i class="fa-solid fa-bell" style="animation: skew-y-shakeing 1.5s infinite;""></i> Réactiver le son ( Notifications ) </button>
 
 
                 <div class="panel panel-default">
