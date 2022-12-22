3004911bf9b6b116218128873a71bb6f7cad5805
Jalil <seth.abdeljalil@gmail.com>
2022-12-22 05:00:09 +0100

    new

--- resources/views/admin/orders/show.blade.php
+++ resources/views/admin/orders/show.blade.php
@@ -133,10 +133,10 @@
                                                         class="fa-solid fa-2x fa-phone-volume slide-phone"></i> Appeler</a> |
                                                     {{ $order->client->phone ?? '' }} | 
                                                     <a href="https://api.whatsapp.com/send?phone=212{{ ltrim($order->client->phone, 'g') }}&text=Bonjour, vous avez effectuez une commande chez TopTopDonuts.ma de {{ $order->total + floatval($order->livraison) ?? '' }} DH, référence de la commande : ( {{$order->ref}} ), Merci de consulter le lien https://{{$host}}/track/{{$order->ref}} pour voir l'état de votre commande."
-                                                            target="_blank"><i class="fa-brands fa-2x  fa-whatsapp  slide-whatsapp"></i> Contacter via Whatsapp avec du contenu</a>
+                                                            target="_blank"><i class="fa-brands fa-2x  fa-whatsapp  slide-whatsapp"></i> Whatsapp (+contenu)</a>
                                                             | 
                                                     <a href="https://api.whatsapp.com/send?phone=212{{ ltrim($order->client->phone, 'g') }}"
-                                                            target="_blank"><i class="fa-brands fa-2x  fa-whatsapp  slide-whatsapp"></i> Contacter via Whatsapp</a>
+                                                            target="_blank"><i class="fa-brands fa-2x  fa-whatsapp  slide-whatsapp"></i> Whatsapp</a>
                                                 </td>
                                             </tr>
                                             <tr>
