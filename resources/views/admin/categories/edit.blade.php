d7397cdafaf8abf6811247094bfb1fec5fcf509c
Jalil <seth.abdeljalil@gmail.com>
2023-02-09 16:52:25 +0100

    fix it

--- resources/views/admin/categories/edit.blade.php
+++ resources/views/admin/categories/edit.blade.php
@@ -84,7 +84,9 @@
                         </div>
                         <div class="form-group {{ $errors->has('image') ? 'has-error' : '' }}">
                             <label for="image">{{ trans('cruds.category.fields.image') }}</label>
-                            <img src="{{$category->image->getUrl() }}" width="100%" />
+                            @isset($category->image)
+                              <img src="{{$category->image->getUrl() }}" width="100%" />
+                            @endisset
                             <hr>
                             <div class="needsclick dropzone" id="image-dropzone">
                             </div>
