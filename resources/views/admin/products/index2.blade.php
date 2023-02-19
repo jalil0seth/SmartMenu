8f8e2709496845d375500af23cff844d55650871
Jalil <seth.abdeljalil@gmail.com>
2023-02-19 02:17:52 +0100

    ->image

--- resources/views/admin/products/index2.blade.php
+++ resources/views/admin/products/index2.blade.php
@@ -77,7 +77,7 @@
                                                 </td>
                                                 <td>
                                                     <div class="input-group">
-                                                        @if (isset($product->image))
+                                                        @if (is_array($product->image))
                                                             <img width="50px" src="{{ $product->image[0]['preview_url'] }}" class="img-circle elevation-2">
                                                         @endif
                                                         {{ $product->name ?? '' }} @if($product->oos == 1) <span class="float-right badge bg-danger">Non disponible</span> @endif
